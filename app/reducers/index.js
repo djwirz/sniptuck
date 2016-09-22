bb645ffa34c8a5c959fa4a1b7848c414d033f289
djwirz <djwirz@gmail.com>
2016-09-22 12:47:32 -0500

    combined reducers to fix snippet test bug

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 
+import snippet from './snippet';
+
 const rootReducer = combineReducers({
+  snippet,
   routing
 });
 
