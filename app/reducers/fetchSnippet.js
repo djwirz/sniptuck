3e19d6072dd15abed1353a9787d1de60c7afda42
djwirz <djwirz@gmail.com>
2016-09-29 14:23:43 -0500

    refactored to use spread operator in reducers, more uniform code structure across reducers

--- app/reducers/fetchSnippet.js
+++ app/reducers/fetchSnippet.js
@@ -6,21 +6,24 @@ const fetchSnippets = (state = {}, action) => {
   switch (action.type) {
     case SNIPPETS_FETCH_REQUEST:
         console.log("SNIPPETS_FETCH_REQUEST")
-        return Object.assign({}, state, {
-            isFetching: true,
-        })
+        return {
+          ...state,
+          isFetching: true,
+        }
     case SNIPPETS_FETCH_SUCCESS:
       console.log("SNIPPETS_FETCH_SUCCESS");
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         newSnippet: action.payload,
-      })
+      }
     case SNIPPETS_FETCH_FAILURE:
       console.log("SNIPPETS_FETCH_FAILURE");
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         newSnippet: null,
-      })
+      }
     default:
       return state
   }
--- app/reducers/snippet.js
+++ app/reducers/snippet.js
@@ -9,21 +9,24 @@ const snippet = (state = {}, action) => {
   switch (action.type) {
     case ADDSNIPPET_REQUEST:
         console.log("ADDSNIPPET_REQUEST")
-        return Object.assign({}, state, {
-            isFetching: true,
-        })
+        return {
+          ...state,
+          isFetching: true,
+        }
     case ADDSNIPPET_SUCCESS:
       console.log("ADDSNIPPET_SUCCESS");
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         newSnippet: action.payload,
-      })
+      }
     case ADDSNIPPET_FAILURE:
       console.log("ADDSNIPPET_FAILURE");
-      return Object.assign({}, state, {
+      return {
+        ...state,
         isFetching: false,
         newSnippet: null,
-      })
+      }
     default:
       return state
   }
